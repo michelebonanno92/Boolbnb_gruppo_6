13dd121a529bc46e2fef6b7aeddca6d29a99cef2
Claudio-Pri <priv.claudio@gmail.com>
2024-11-24 16:50:31 +0100

    spostato controller service in admin

--- app/Http/Controllers/Admin/ServiceController.php
+++ app/Http/Controllers/Admin/ServiceController.php
@@ -1,8 +1,10 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
 use App\Models\Service;
+use App\Http\Controllers\Controller;
+
 use Illuminate\Http\Request;
 
 class ServiceController extends Controller
