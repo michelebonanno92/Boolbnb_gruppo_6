fc0e20b726be5a54c47ace4f260c1b8b2eb47050
Claudio-Pri <priv.claudio@gmail.com>
2024-11-24 18:29:48 +0100

    modifiche views index-show-edit//modificate migration per abilitare delete// css

--- resources/views/admin/apartments/show.blade.php
+++ resources/views/admin/apartments/show.blade.php
@@ -1,67 +1,113 @@
 @extends('layouts.app')
 
-@section('page-title', $apartment->name )
+@section('page-title', $apartment->slug )
 
 @section('main-content')
 
-  <div class="card w-100 mb-3 text-center">
-    <div class="card-body">
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+				<div class="card py-2 px-4">
+					<h4>
+						{{ $apartment->title }}
+					</h4>
+					<p>
+						{{ $apartment->description }}
+					</p>
+					<ul>
+						<li>
+							Stanze: {{ $apartment->rooms }}
+						</li>
+						<li>
+							Letti: {{ $apartment->beds }}
+						</li>
+						<li>
+							Bagni: {{ $apartment->toilets }}
+						</li>
+					</ul>
 
-      <div class="col-12 d-flex align-content-between">
-        <div class="card mb-4 card-apartment  ">
-            <img src="{{ $apartment->image }}" class="card-img-top" alt="{{ $apartment->title }}">
-            <div class="card-body">
-              <h3 class="card-title">{{ $apartment->title }}</h3>
-              <p class="card-text">{{ $apartment->address }}</p>
-              <div class="row">
-                <div class="col-3">
-                  <h4>Stanze</h4>
-                  <h5>{{ $apartment->rooms }}</h5>
-                </div>
-                <div class="col-3">
-                  <h4>Letti</h4>
-                  <h5>{{ $apartment->beds }}</h5>
-                </div>
-                <div class="col-3">
-                  <h4>Bagni</h4>
-                  <h5>{{ $apartment->toilets }}</h5>
-                </div>
-                <div class="col-3">
-                  <h4>Metri quadri</h4>
-                  <h5>{{ $apartment->square_meters }}</h5>
-                </div>
-              </div>
-              @if ( $apartment->visible == 1)
-                <div>
-                  <p class="text-success">Visibile</p>
-                </div>
-              @else
-                <div>
-                  <p class="text-danger">Non Visibile</p>
-                </div>
-              @endif
+					<div>
+						Servizi:
+						<ul>
+							<li>
+								placeholder servizi
+							</li>
+						</ul>
+					</div>
 
-              {{-- //messaggi --}}
-              {{-- <div>
-                <a href="message" class="btn btn-success">Messaggi</a>
-              </div> --}}
-            </div>
-          </div>
-    </div>
+					<div class="card-image">
+						placeholder immagine
+					</div>
 
-      <a href="{{ route('admin.apartments.edit', ['apartment' => $apartment->id]) }}" class="btn btn-warning">Modifica</a>
-      <form 
-        {{-- aggiunto conferma di cancellazione --}}
-          onsubmit="return confirm('sei sicuro di volerlo cancellare ?')"
-          action="{{ route('admin.apartments.destroy', ['apartment' => $apartment->id]) }}" 
-          method="POST" 
-          class="d-inline-block">
-          @csrf
-          @method('DELETE')
-          <button type="submit" class="btn btn-danger">
-              Elimina
-          </button>
-      </form>
-    </div>
-  </div>
+					<div>
+						<a href="{{ route('admin.apartments.edit', ['apartment' => $apartment->id ]) }}">Modifica</a>
+					</div>
+					<form 
+						onsubmit="return confirm('Sei sicuro di voler cancellare questo appartamento?')"
+						action="{{ route('admin.apartments.destroy', ['apartment' => $apartment->id]) }}" 
+						method="POST" 
+						class="d-inline-block">
+						@csrf
+						@method('DELETE')
+						<button type="submit" class="btn btn-danger">
+							Elimina
+						</button>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+
 @endsection
+
+<!-- <div class="card w-100 mb-3 text-center">
+	<div class="card-body">
+
+		<div class="col-12 d-flex align-content-between">
+			<div class="card mb-4 card-apartment  ">
+			<img src="{{ $apartment->image }}" class="card-img-top" alt="{{ $apartment->title }}">
+			<div class="card-body">
+				<h3 class="card-title">{{ $apartment->title }}</h3>
+				<p class="card-text">{{ $apartment->address }}</p>
+				<div class="row">
+				<div class="col-3">
+					<h4>Stanze</h4>
+					<h5>{{ $apartment->rooms }}</h5>
+				</div>
+				<div class="col-3">
+					<h4>Letti</h4>
+					<h5>{{ $apartment->beds }}</h5>
+				</div>
+				<div class="col-3">
+					<h4>Bagni</h4>
+					<h5>{{ $apartment->toilets }}</h5>
+				</div>
+				<div class="col-3">
+					<h4>Metri quadri</h4>
+					<h5>{{ $apartment->square_meters }}</h5>
+				</div>
+				</div>
+				@if ( $apartment->visible == 1)
+				<div>
+					<p class="text-success">Visibile</p>
+				</div>
+				@else
+				<div>
+					<p class="text-danger">Non Visibile</p>
+				</div>
+				@endif
+
+				{{-- //messaggi --}}
+				{{-- <div>
+				<a href="message" class="btn btn-success">Messaggi</a>
+				</div> --}}
+			</div>
+			</div>
+		</div>
+
+
+	</div>
+</div> -->
+
+        <!-- <a href="{{ route('admin.apartments.edit', ['apartment' => $apartment->id]) }}" class="btn btn-warning">Modifica</a> -->
+        
