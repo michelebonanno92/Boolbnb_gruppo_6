48f5c18b7a056e08700981268a0f88fcf6d97b91
Claudio-Pri <priv.claudio@gmail.com>
2024-11-25 15:35:15 +0100

    correzione lingua login-registrazione-errore in login/ registrazione// modifica form e controller registrazione

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,46 +3,107 @@
 @section('main-content')
     <form method="POST" action="{{ route('register') }}">
         @csrf
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
 
         <!-- Name -->
-        <div>
-            <label for="name">
-                Name
-            </label>
-            <input type="text" id="name" name="name">
+        <div class="row mb-2">
+            <div class="col-12 col-md-6 col-lg-1">
+                <label for="name">
+                    Nome<span class="text-danger">*</span>
+                </label>
+            </div>
+        </div>
+        <div class="row mb-2">
+            <div class="col-12 col-md-6 col-lg-2">
+                <input type="text" id="name" name="name" required min="3" max="64">
+            </div>
+        </div>
+
+        <!-- Surname -->
+        <div class="row mb-2">
+            <div class="col-12 col-md-6 col-lg-1">
+                <label for="surname">
+                    Cognome<span class="text-danger">*</span>
+                </label>
+            </div>
+        </div>
+        <div class="row mb-2">
+            <div class="col-12 col-md-6 col-lg-2">
+                <input type="text" id="surname" name="surname" required min="3" max="64">
+            </div>
+        </div>
+        
+        <!-- Date of birth -->
+        <div class="row mb-2">
+            <div class="col-12 col-md-6 col-lg-2">
+                <label for="date_of_birth">
+                    Data di nascita<span class="text-danger">*</span>
+                </label>
+            </div>
+        </div>
+        <div class="row mb-2">
+            <div class="col-12 col-md-6 col-lg-2">
+                <input type="date" id="date_of_birth" name="date_of_birth" required>
+            </div>
         </div>
 
         <!-- Email Address -->
-        <div class="mt-4">
-            <label for="email">
-                Email
-            </label>
-            <input type="email" id="email" name="email">
+        <div class="row mb-2">
+            <div class="col-12 col-md-6 col-lg-1">
+                <label for="email">
+                    Email<span class="text-danger">*</span>
+                </label>
+            </div>
+        </div>
+        <div class="row mb-2">
+            <div class="col-12 col-md-6 col-lg-2">
+                <input type="email" id="email" name="email" required>
+            </div>
         </div>
 
         <!-- Password -->
-        <div class="mt-4">
-            <label for="password">
-                Password
-            </label>
-            <input type="password" id="password" name="password">
+        <div class="row mb-2">
+            <div class="col-12 col-md-6 col-lg-1">
+                <label for="password">
+                    Password<span class="text-danger">*</span>
+                </label>
+            </div>
         </div>
-
+        <div class="row mb-2">
+            <div class="col-12 col-md-6 col-lg-2">
+                <input type="password" id="password" name="password" required>
+            </div>
+        </div>
+        
         <!-- Confirm Password -->
-        <div class="mt-4">
-            <label for="password_confirmation">
-                Conferma Password
-            </label>
-            <input type="password" id="password_confirmation" name="password_confirmation">
+        <div class="row mb-2">
+            <div class="col-12 col-md-6 col-lg-2">
+                <label for="password_confirmation">
+                    Conferma Password<span class="text-danger">*</span>
+                </label>
+            </div>
+        </div>
+        <div class="row mb-2">
+            <div class="col-12 col-md-6 col-lg-2">
+                <input type="password" id="password_confirmation" name="password_confirmation" required>
+            </div>
         </div>
 
-        <div>
-            <a href="{{ route('login') }}">
-                {{ __('Already registered?') }}
+        <div class="py-2">
+            <a href="{{ route('login') }}" class="me-2 btn btn-outline-primary">
+                {{ __('Già registrato?') }}
             </a>
 
-            <button type="submit">
-                Register
+            <button type="submit" class="btn btn-success">
+                Registrati
             </button>
         </div>
     </form>
