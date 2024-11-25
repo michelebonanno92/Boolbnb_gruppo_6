010663aa701557a1c455cbfd775e66a3aefe816e
claudiopatti <claudiopatti1997@gmail.com>
2024-11-25 17:46:22 +0100

    aggiunto api - Apartment

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+//Controller
+use App\Http\Controllers\API\ApartmentController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +17,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::name('api.')->group(function () {
+
+    Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+        return $request->user();
+    });
+    
+    Route::get('/apartments', [ApartmentController::class, 'index'])->name('apartments.index');
+
 });
