15d303bed1f08e3cede657aed6f476e6e4964108
ChristopherS24 <chryskies@gmail.com>
2024-11-28 12:39:30 +0100

    data e surname validati

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -37,13 +37,13 @@
         <div class="row mb-2">
             <div class="col-12 col-md-6 col-lg-1">
                 <label for="surname">
-                    Cognome<span class="text-danger">*</span>
+                    Cognome<span class="text-danger"></span>
                 </label>
             </div>
         </div>
         <div class="row mb-2">
             <div class="col-12 col-md-6 col-lg-2">
-                <input type="text" id="surname" name="surname" required min="3" max="64">
+                <input type="text" id="surname" name="surname" min="3" max="64">
             </div>
         </div>
         
