c4003eadde530c0b120fa831aa591bec9b97282f
Claudio-Pri <priv.claudio@gmail.com>
2024-11-28 13:03:55 +0100

    turbo-fix registrazione-css

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,116 +1,90 @@
 @extends('layouts.guest')
 
 @section('main-content')
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
-        @if ($errors->any())
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
-        <div class="row mb-2">
-            <div class="col-12 col-md-6 col-lg-2">
-                <label for="name">
-                    <span class="text-danger">*</span>Campi obbligatori
-                </label>
-            </div>
-        </div>
-        <!-- Name -->
-        <div class="row mb-2">
-            <div class="col-12 col-md-6 col-lg-1">
-                <label for="name">
-                    Nome<span class="text-danger">*</span>
-                </label>
-            </div>
-        </div>
-        <div class="row mb-2">
-            <div class="col-12 col-md-6 col-lg-2">
-                <input type="text" id="name" name="name" required min="3" max="64">
-            </div>
-        </div>
-
-        <!-- Surname -->
-        <div class="row mb-2">
-            <div class="col-12 col-md-6 col-lg-1">
-                <label for="surname">
-                    Cognome<span class="text-danger"></span>
-                </label>
-            </div>
-        </div>
-        <div class="row mb-2">
-            <div class="col-12 col-md-6 col-lg-2">
-                <input type="text" id="surname" name="surname" min="3" max="64">
-            </div>
-        </div>
-        
-        <!-- Date of birth -->
-        <div class="row mb-2">
-            <div class="col-12 col-md-6 col-lg-2">
-                <label for="date_of_birth">
-                    Data di nascita<span class="text-danger">*</span>
-                </label>
-            </div>
-        </div>
-        <div class="row mb-2">
-            <div class="col-12 col-md-6 col-lg-2">
-                <input type="date" id="date_of_birth" name="date_of_birth" required>
-            </div>
-        </div>
-
-        <!-- Email Address -->
-        <div class="row mb-2">
-            <div class="col-12 col-md-6 col-lg-1">
-                <label for="email">
-                    Email<span class="text-danger">*</span>
-                </label>
-            </div>
-        </div>
-        <div class="row mb-2">
-            <div class="col-12 col-md-6 col-lg-2">
-                <input type="email" id="email" name="email" required>
-            </div>
-        </div>
-
-        <!-- Password -->
-        <div class="row mb-2">
-            <div class="col-12 col-md-6 col-lg-1">
-                <label for="password">
-                    Password<span class="text-danger">*</span>
-                </label>
-            </div>
-        </div>
-        <div class="row mb-2">
-            <div class="col-12 col-md-6 col-lg-2">
-                <input type="password" id="password" name="password" required>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-6 offset-md-3">
+                <div class="card form-card p-4">
+                    <form method="POST" action="{{ route('register') }}">
+                        @csrf
+                        @if ($errors->any())
+                            <div class="alert alert-danger">
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+                        <div>
+                            <label for="name"  class="form-label fw-bold">
+                                <span class="text-danger">*</span>Campi obbligatori
+                            </label>
+                        </div>
+                        <!-- Name -->
+                        <label for="name" class="form-label fw-bold">
+                            Nome<span class="text-danger">*</span>
+                        </label>
+                        <input type="text" class="form-control" id="name" name="name" required min="3" max="64">
+                        
+                
+                        <!-- Surname -->
+                        <div>
+                            <label for="surname" class="form-label fw-bold">
+                                Cognome<span class="text-danger"></span>
+                            </label>
+                        </div>
+                        <input type="text" class="form-control" id="surname" name="surname" min="3" max="64">
+                        
+                        
+                        <!-- Date of birth -->
+                        <div>
+                            <label for="date_of_birth" class="form-label fw-bold">
+                                Data di nascita<span class="text-danger">*</span>
+                            </label>
+                        </div>
+                        <input type="date" class="form-control" id="date_of_birth" name="date_of_birth" required>
+                        
+                
+                        <!-- Email Address -->
+                        <div>
+                            <label for="email" class="form-label fw-bold">
+                                Email<span class="text-danger">*</span>
+                            </label>
+                        </div>
+                        <input type="email" class="form-control" id="email" name="email" required>
+                        
+                
+                        <!-- Password -->
+                        <div>
+                            <label for="password" class="form-label fw-bold">
+                                Password<span class="text-danger">*</span>
+                            </label>
+                        </div>
+                        <input type="password" class="form-control" id="password" name="password" required>
+                        
+                        
+                        <!-- Confirm Password -->
+                        <div>
+                            <label for="password_confirmation" class="form-label fw-bold">
+                                Conferma Password<span class="text-danger">*</span>
+                            </label>
+                        </div>
+                        <input type="password" class="form-control mb-2" id="password_confirmation" name="password_confirmation" required>
+                        
+                
+                        <div class="py-2">
+                            <a href="{{ route('login') }}" class="me-2 btn btn-outline-primary">
+                                {{ __('Già registrato?') }}
+                            </a>
+                
+                            <button type="submit" class="btn btn-success">
+                                Registrati
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
-        
-        <!-- Confirm Password -->
-        <div class="row mb-2">
-            <div class="col-12 col-md-6 col-lg-2">
-                <label for="password_confirmation">
-                    Conferma Password<span class="text-danger">*</span>
-                </label>
-            </div>
-        </div>
-        <div class="row mb-2">
-            <div class="col-12 col-md-6 col-lg-2">
-                <input type="password" id="password_confirmation" name="password_confirmation" required>
-            </div>
-        </div>
-
-        <div class="py-2">
-            <a href="{{ route('login') }}" class="me-2 btn btn-outline-primary">
-                {{ __('Già registrato?') }}
-            </a>
-
-            <button type="submit" class="btn btn-success">
-                Registrati
-            </button>
-        </div>
-    </form>
+    </div>
 @endsection
