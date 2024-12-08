0cc7739afacdf13f731d9107ef07547123ea3371
claudiopatti <claudiopatti1997@gmail.com>
2024-12-08 16:40:47 +0100

    aggiornata show

--- resources/views/admin/apartments/show.blade.php
+++ resources/views/admin/apartments/show.blade.php
@@ -6,72 +6,79 @@
 
 	<div class="container  text-center">
 		<div class="row ">
-			<div class="col-12 col-md-6 offset-md-3">
-				<div class="card p-4">
-					<div>
+			<div class="col-12 col-md-8 offset-md-2">
+				<div class="p-4">
+					<div class="col-12 col-md-6 offset-md-3">
 						@if ($apartment->image)
 						  <img src="{{ '/storage/'.$apartment->image }}" alt="{{ $apartment->title }}" class="img-fluid rounded">
 						{{-- @else
 						  <img src="#" alt="da caricare" class="my-img rounded"> --}}
 						@endif
 					</div>
-					<h4 class="mt-4">
+					<h4 class="mt-4 ">
 						{{ $apartment->title }}
 					</h4>
-					<div class="mb-3">
-						@if ($apartment->visible)
-							<div class="badge text-bg-success">
-								Pubblicato
-							</div>
-
-						@else
-							<div class="badge text-bg-warning">
-								Non pubblicato
-							</div>
-						@endif
-					</div>
-					<div class="mb-2">
-						@if($apartment->sponsorships->count())
-							@if($apartment->sponsorships->count() > 1)
-								<i class="fa-solid fa-bolt text-warning"></i>
-								<strong class="fs-4">
-									{{ $apartment->sponsorships->sum('duration_hours') }}
-								</strong>
-								<strong class="fs-4">ore</strong>
+					<div class="d-flex justify-content-center mb-3">
+						<div class="me-3">
+							@if ($apartment->visible)
+	
+								<i class="fa-solid text-success fa-2x fa-eye"></i>
+	
 							@else
-								<i class="fa-solid fa-bolt text-warning"></i>
-								<strong class="fs-4">{{ $apartment->sponsorships->first()->duration_hours }}</strong>
-								<span class="fs-4">ore</span>
+	
+								<i class="fa-solid text-danger fa-2x fa-eye-slash"></i>
+	
 							@endif
-						@endif
-					</div>
+							
+						</div>
+						<div>
+							@if($apartment->sponsorships->count())
+								@if($apartment->sponsorships->count() > 1)
+									<i class="fa-solid fa-bolt text-warning"></i>
+									<strong class="fs-4">
+										{{ $apartment->sponsorships->sum('duration_hours') }}
+									</strong>
+									<strong class="fs-4">ore</strong>
+								@else
+									<i class="fa-solid fa-bolt text-warning"></i>
+									<strong class="fs-4">{{ $apartment->sponsorships->first()->duration_hours }}</strong>
+									<span class="fs-4">ore</span>
+								@endif
+							@endif
+							
+						</div>
+		            </div>
+					{{-- <div class="mb-2">
+					</div> --}}
 					<div>
 						<a href="{{ route('admin.sponsorships.index', ['apartment_id' => $apartment->id]) }}"  class="btn btn-outline-primary fs-4 fw-semibold w-100 mb-4">
 							Sponsorizza
 						</a>
 					</div>
 					<p>
+						<i class="fa-solid fa-location-dot text-danger"></i>
 						{{ $apartment->address }}
 					</p>
-					<p class="text-start">
+					<p class="text-start text-secondary fw-bold">
 						{{ $apartment->description }}
 					</p>
 					<ul class="text-start">
 						<li>
-							Stanze: {{ $apartment->rooms }}
+							Stanze: <span class="fw-bold">{{ $apartment->rooms }}</span>
 						</li>
 						<li>
-							Letti: {{ $apartment->beds }}
+							Letti: <span class="fw-bold">{{ $apartment->beds }}</span>
 						</li>
 						<li>
-							Bagni: {{ $apartment->toilets }}
+							Bagni: <span class="fw-bold">{{ $apartment->toilets }}</span>
 						</li>
 					</ul>
 
 					<div class="text-start mb-2">
+						<h3>I tuoi servizi:</h3>
 						<ul class="p-0">
 							@foreach ($apartment->services as $service)
-								<li class="badge text-dark my-services fs-6 me-2 p-0">
+								<li class="text-secondary my-services fs-6 me-2 p-0 fw-bold ">
 									{{ $service->service_name }}
 								</li>
                         	@endforeach
@@ -85,7 +92,7 @@
 							</div>
 
 							<div class="d-inline-block me-3">
-								<a href="{{ route('admin.apartments.edit', ['apartment' => $apartment->id ]) }}"  class="btn btn-outline-warning mb-4">Modifica</a>
+								<a href="{{ route('admin.apartments.edit', ['apartment' => $apartment->id ]) }}"  class="btn btn-outline-primary mb-4">Modifica</a>
 							</div>
 							
 							<form 
